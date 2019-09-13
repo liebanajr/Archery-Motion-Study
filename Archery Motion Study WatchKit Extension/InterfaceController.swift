e56ebbe478db444497f16ebafb2b09b32be0fdc6
Juan I Rodriguez <liebanajr@icloud.com>
2019-09-13 13:47:21 +0200

    Initial Commit

---
+++ Archery Motion Study WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  Archery Motion Study WatchKit Extension
+//
+//  Created by Juan Ignacio Rodríguez Liébana on 13/09/2019.
+//  Copyright © 2019 liebanajr. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
---
+++ Archery Motion Study/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Archery Motion Study
+//
+//  Created by Juan Ignacio Rodríguez Liébana on 13/09/2019.
+//  Copyright © 2019 liebanajr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
