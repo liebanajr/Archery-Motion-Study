d74191013c8fcc0190b1e461ee7af5033b61b11b
Juan I Rodriguez <juanrodriguezliebana@gmail.com>
2019-10-04 10:15:32 -0500

    Implemented sample average to smooth data before presenting chart

--- Archery Motion Study/Model/SampleAverageManager.swift
+++ Archery Motion Study/Model/SampleAverageManager.swift
@@ -19,16 +19,36 @@ class SampleAverageManager : NSObject {
     init(nSamples : Int){
         
         self.nSamples = nSamples
-        self.samples = [Double]()
+        self.samples = Array(repeating: 0.0, count: nSamples)
         self.currentValue = 0
-        
-        for i in 0...(nSamples-1) {
-            
-            self.samples[i] = 0
-            
-        }
+
     }
     
+    func calculateNewAverage(newSample: Double) -> Double {
+        
+        addNewSampleToArray(newSample: newSample)
+        let sum = samples.reduce(0, { x, y in
+            x + y
+        })
+        
+        currentValue = sum / Double(nSamples)
+        
+        return currentValue
+    }
+    
+    func addNewSampleToArray(newSample: Double){
+        
+        
+        for i in 0...(samples.count-2) {
+            
+//            Array shiftes to the left and adds new sample at the last index
+            samples[i] = samples [i+1]
+            
+        }
+//        New sample is added to the last index
+        samples[samples.count-1] = newSample
+        
+    }
     
     
 }
