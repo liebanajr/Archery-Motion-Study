d272de752d9dc84b36ecc20c8e9cb55b8eecafe3
Juan I Rodriguez <juanrodriguezliebana@gmail.com>
2019-10-12 14:43:27 -0500

    Added multiple level filter to chart view

--- Archery Motion Study/Controller/ChartViewController.swift
+++ Archery Motion Study/Controller/ChartViewController.swift
@@ -10,20 +10,6 @@ import UIKit
 import Charts
 import Accelerate
 
-struct LowPassFilterSignal {
-    /// Current signal value
-    var value: Double
-    
-    /// A scaling factor in the range 0.0..<1.0 that determines
-    /// how resistant the value is to change
-    let filterFactor: Double
-
-    /// Update the value, using filterFactor to attenuate changes
-    mutating func update(newValue: Double) {
-        value = filterFactor * value + (1.0 - filterFactor) * newValue
-    }
-}
-
 class ChartViewController: UIViewController {
         
     @IBOutlet weak var chtChart: LineChartView!
@@ -43,6 +29,8 @@ class ChartViewController: UIViewController {
     var availableDataSets : [SensorDataSet]?
     
     var desiredDataSets : [SensorDataSet]?
+    
+    var averageManager : SampleAverageManager?
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -61,6 +49,8 @@ class ChartViewController: UIViewController {
         timeStamp = availableDataSets?.remove(at: 0)
         
         desiredDataSets = []
+        
+        averageManager = SampleAverageManager(nSamples: 10, filterLevel: 4)
         
         chtChart.dragXEnabled = true
         chtChart.dragYEnabled = false
@@ -120,13 +110,13 @@ class ChartViewController: UIViewController {
                 
                 var lineChartEntry = [ChartDataEntry]()
                 
-                var smoothData = LowPassFilterSignal(value: 0, filterFactor: 0.9)
-                
-                for (index,value) in dataSet.data.enumerated() {
+//                Average sensor data
+                let smoothData = averageManager?.averageSignal(inputSignal: dataSet.data)
+                       
+                for (index,value) in smoothData!.enumerated() {
                     
-                    let entry = ChartDataEntry(x: timeStamp!.data[index], y: smoothData.value)
+                    let entry = ChartDataEntry(x: timeStamp!.data[index], y: value)
                     lineChartEntry.append(entry)
-                    smoothData.update(newValue: value)
                 }
                 
                 let line1 = LineChartDataSet(entries: lineChartEntry, label: dataSet.label)
