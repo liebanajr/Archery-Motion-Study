322aca8187acc1634e7085db9989045694ed9573
Juan I Rodriguez <juanrodriguezliebana@gmail.com>
2019-11-14 21:13:30 +0100

    Delete session confirmation

--- Archery Motion Study/Controller/ChartViewController.swift
+++ Archery Motion Study/Controller/ChartViewController.swift
@@ -222,7 +222,7 @@ class ChartViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
-    
+        
     func toggleDeviceOrientation() {
         
         var value : Int = UIInterfaceOrientation.landscapeRight.rawValue
