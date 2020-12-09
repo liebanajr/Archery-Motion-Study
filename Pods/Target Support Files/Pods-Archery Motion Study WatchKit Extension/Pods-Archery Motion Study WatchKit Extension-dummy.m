4d93f1ccbc7d2f8d44ca1981bad4cb50a28be310
Juan Rodriguez <juanrodriguezliebana@icloud.com>
2020-12-09 13:39:32 +0100

    Using SPM instead of CocoaPods

--- Pods/Target Support Files/Pods-Archery Motion Study WatchKit Extension/Pods-Archery Motion Study WatchKit Extension-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Archery_Motion_Study_WatchKit_Extension : NSObject
-@end
-@implementation PodsDummy_Pods_Archery_Motion_Study_WatchKit_Extension
-@end
--- Pods/Target Support Files/Pods-Archery Motion Study/Pods-Archery Motion Study-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Archery_Motion_Study : NSObject
-@end
-@implementation PodsDummy_Pods_Archery_Motion_Study
-@end
